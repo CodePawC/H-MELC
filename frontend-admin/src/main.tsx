dee974f740a3ae6364b9c8c195f4fee8707fb159
CodePawC <225864876+CodePawC@users.noreply.github.com>
2026-06-04 12:46:23 +0800

    fix: remove unused Navigate import

--- frontend-admin/src/main.tsx
+++ frontend-admin/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Navigate } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { App as AntdApp, ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 
